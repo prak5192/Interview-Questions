1a9910b426deb28efd658411cf70707fec3410cf
Prak <www.prak.co.in@gmail.com>
2013-05-24 08:23:23 +0530

    Added a example of strtoul function

---
+++ C/strtoul.c
@@ -0,0 +1,17 @@
+/* Example to show the strtoul function */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+void main(void);
+void main(void)
+{
+     char* str  = "12345ascd";
+     unsigned long num;
+     char* leftover;
+     num = strtoul(str, &leftover, 10);
+     printf("Original string:      %s\n", str);
+     printf("Converted number:     %lu\n", num);
+     printf("Leftover characters:  %s\n", leftover);
+}
