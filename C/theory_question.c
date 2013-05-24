9f61980ebe9e6c880c815cf2fcfbfb5bb54dcbe6
Prak <www.prak.co.in@gmail.com>
2013-05-24 08:33:29 +0530

    printf function example

--- C/theory_question.c
+++ C/theory_question.c
@@ -8,7 +8,8 @@ function is designed to work with any type of data.
 
 Because not all data ends with a null character, you must provide the memcpy() 
 function with the number of bytes you want to copy from the source to the 
-destination. The following program shows examples of both the strcpy() and the memcpy() functions:
+destination. The following program shows examples of both the strcpy() and 
+the memcpy() functions:
 
 
 #include <stdio.h>
@@ -45,5 +46,96 @@ void main(void)
                dest_cust.id, dest_cust.first_name, dest_cust.last_name);
 }
 
+===============================================================================
 
+2. How can I remove the trailing spaces from a string?
+
+The C language does not provide a standard function that removes trailing spaces 
+from a string. It is easy, however, to build your own function to do just this. 
+The following program uses a custom function named rtrim() to remove the trailing 
+spaces from a string. It carries out this action by iterating through the string 
+backward, starting at the character before the terminating null character (\0) and 
+ending when it finds the first nonspace character. When the program finds a nonspace 
+character, it sets the next character in the string to the terminating null character 
+(\0), thereby effectively eliminating all the trailing blanks. Here is how 
+this task is performed:
+
+#include <stdio.h>
+#include <string.h>
+void main(void);
+char* rtrim(char*);
+void main(void)
+{
+     char* trail_str = "This string has trailing spaces in it.               ";
+     /* Show the status of the string before calling the rtrim()
+        function. */
+     printf("Before calling rtrim(), trail_str is '%s'\n", trail_str);
+     printf("and has a length of %d.\n", strlen(trail_str));
+     /* Call the rtrim() function to remove the trailing blanks. */
+     rtrim(trail_str);
+     /* Show the status of the string
+        after calling the rtrim() function. */
+     printf("After calling rtrim(), trail_str is '%s'\n", trail_str);
+     printf("and has a length of %d.\n", strlen(trail_str));
+}
+/* The rtrim() function removes trailing spaces from a string. */
+char* rtrim(char* str)
+{
+int n = strlen(str) - 1;     /* Start at the character BEFORE
+                                     the null character (\0). */
+     while (n>0)            /* Make sure we don't go out of bounds... */
+     {
+          if (*(str+n) != ' ')    /*  If we find a nonspace character: */
+          {
+               *(str+n+1) = '\0'; /* Put the null character at one
+                                     character past our current
+                                     position. */
+               break;             /* Break out of the loop. */
+          }
+          else      /* Otherwise, keep moving backward in the string. */
+               n--;
+     }
+     return str;                  /* Return a pointer to the string. */
+}
+
+===============================================================================
+
+3. How do you print only part of a string?
+
+The following program shows how to print only part of a string using the printf() 
+function:
+
+#include <stdio.h>
+#include <string.h>
+void main(void);
+void main(void)
+{
+     char* source_str = "THIS IS THE SOURCE STRING";
+     /* Use printf() to print the first 11 characters of source_str. */
+     printf("First 11 characters: '%11.11s'\n", source_str);
+     /* Use printf() to print only the
+        last 13 characters of source_str. */
+     printf("Last 13 characters: '%13.13s'\n",
+                 source_str + (strlen(source_str) - 13));
+}
+
+This example program produces the following output:
+
+First 11 characters: 'THIS IS THE'
+
+Last 13 characters: 'SOURCE STRING'
+
+The first call to printf() uses the argument "%11.11s" to force the printf() 
+function to make the output exactly 11 characters long. Because the source 
+string is longer than 11 characters, it is truncated, and only the first 11 
+characters are printed. The second call to printf() is a bit more tricky. 
+The total length of the source_str string is calculated (using the strlen() 
+function). Then, 13 (the number of characters you want to print) is subtracted 
+from the total length of source_str.
+
+This gives the number of remaining characters in source_str. This number is then 
+added to the address of source_str to give a pointer to an address in the source 
+string that is 13 characters from the end of source_str. By using the argument 
+"%13.13s", the program forces the output to be exactly 13 characters long, and 
+thus the last 13 characters of the string are printed.
 
