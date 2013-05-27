9cac51e41797cd555eba73671a3c113edb6a87a4
Prak <www.prak.co.in@gmail.com>
2013-05-27 08:24:10 +0530

    Added a program to convert little endian to bin endian

--- C/little_bigendian.c
+++ C/little_bigendian.c
@@ -3,20 +3,34 @@ Program to convert little endian to big endian.
 */
 
 #include<stdio.h>
+#include<arpa/inet.h>
 
+/* function to show bytes in memory, from location start to start+n*/
+void show_mem_rep(char *start, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+         printf(" %.2X", start[i]);
+    printf("\n");
+}
 
 int main(void){
-	unsigned int little_endian_num = 0xABCDEFDE;
-	unsigned int big_endian_num;
+	int little_endian_num = 0x12345678;
+	int big_endian_num;
+	big_endian_num = (( little_endian_num & 0xFF000000) >> 24 ) | 
+					 (( little_endian_num & 0x00FF0000) >> 8 ) | 
+					 (( little_endian_num & 0x0000FF00) << 8 ) | 
+					 (( little_endian_num & 0x000000FF) << 24 );
 
-	big_endian_num = (( little_endian_num >> 24 ) & 0xFF)| 
-					 (( little_endian_num >> 8 ) & 0xFF) | 
-					 (( little_endian_num << 8 ) & 0xFF) | 
-					 (( little_endian_num << 24 ) & 0xFF);
 
-	printf("Little Endian : %X\n",little_endian_num);
-	printf("Big Endian : %X\n",big_endian_num);
+    printf("Little endian:");
+    show_mem_rep((char*)&little_endian_num,sizeof(little_endian_num));
+    printf("Big endian:");
+    show_mem_rep((char*)&big_endian_num,sizeof(big_endian_num));
 
+    printf("By using htonl() function\n");
+    big_endian_num = htonl(little_endian_num);
+    printf("Big endian:");
+    show_mem_rep((char*)&big_endian_num,sizeof(big_endian_num));
 	return 0;
 }
-	
