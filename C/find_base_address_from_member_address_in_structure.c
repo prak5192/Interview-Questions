fd84616f3f207791e9d74c830dd041812d5db9d0
Prak <www.prak.co.in@gmail.com>
2013-06-11 09:12:18 +0530

    Successfully wrote the function for finding the base address of a structure from its member address

--- C/find_base_address_from_member_address_in_structure.c
+++ C/find_base_address_from_member_address_in_structure.c
@@ -8,6 +8,10 @@ of the structure..
 
 #endif
 
+#include<stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 
 typedef struct A{
     int a;
@@ -17,18 +21,42 @@ typedef struct A{
 
 
 
+int foo(int *ptr)
+{
+    long int offset;
+    my_struct *base_address;
+ 
 
-int foo(int *ptr){
+    printf("Offset calculated by the offsetof macro\n");   
+    long int value = (long)offsetof(my_struct,c); 
+    printf("Offset value %ld\n",value);
+
+    printf("Offset calculated manually\n");   
+    offset = (char *)&(((my_struct *)0)->c) - ((char *)(my_struct *)0);
+    printf("Offset value %ld\n",value);
+
+
+    printf("ptr = %p\n",ptr);
+    base_address = (my_struct *) ((long int)ptr - offset);
+    printf("base_address = %p\n",base_address);
+
+    printf("The value in structure in foo function\n");
+    printf("a = %d\t b = %c\t c = %d\n",base_address->a, base_address->b, base_address->c);
     
-    // Find the base address of the structure
-
+    base_address->a = 60;
+    base_address->b = 'P';
+    base_address->c = 100;
+    
+    return 0;
 }
 
-
-int main(void){
+int main(void)
+{
     my_struct hello = {20,'Z',34};
 
-    ret = foo(&hello.a);
-
-    retutn 0;
+    int ret = foo(&hello.c);
+    printf("The value in structure after calling modification function\n");
+    printf("a = %d\t b = %c\t c = %d\n",hello.a, hello.b, hello.c);
+    
+    return 0;
 }
