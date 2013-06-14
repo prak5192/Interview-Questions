d9d2ea0dfb99c8ea8eec7ab1534cdee01e02cb17
Prak <www.prak.co.in@gmail.com>
2013-06-14 09:07:27 +0530

    Renamed the delete_a_given_node_in_linked_list.c

---
+++ C/delete_a_given_node_in_linked_list_todo.c
@@ -0,0 +1,15 @@
+#if 0
+Given only a pointer to a node to be deleted in 
+a singly linked list, how do you delete it?
+
+The solution to this is to copy the data from 
+the next node into this node and delete the 
+next node!. Ofcourse this wont work if the 
+node to be deleted is the last node. Mark it 
+as dummy in that case. If you have a Circular 
+linked list, then this might be all the more 
+interesting. Try writing your own C program to 
+solve this problem. Having a doubly linked 
+list is always better. 
+
+#endif
