5c2d61643fc8cd711780e6a6de6f72c8e8fa97c5
Prak <www.prak.co.in@gmail.com>
2013-06-17 09:26:31 +0530

    Added file read and write in the function

--- C/sorted_linked_list_todo.c
+++ C/sorted_linked_list_todo.c
@@ -38,12 +38,10 @@ void Insert(person **head)
         name[i] = c;
         i++;
         if(i == (MAX_CHAR -1) ){
-            printf("Inside break statement i = %d\n", i);
             name[i] = '\0';
             while ((c = getchar()) != '\n' && c != EOF);
             break;
         }
-        printf("i = %d\n", i);
     }
     printf("Name entered: %s\n",name);
     memcpy(new->name , name, MAX_CHAR); 
@@ -70,11 +68,10 @@ void Print(person **head)
     int i = 1;
      
     printf("\n\tLIST CONTENTS\n\n");
-    if (temp == NULL) printf("List is empty!!");
-    else
-    {
-        while (temp != NULL)
-        {
+    if (temp == NULL){
+        printf("List is empty!!");
+    } else {
+        while (temp != NULL){
             printf("No: %d\tNAME: %s\tAGE: %d\n", i, temp->name, temp->age);
             temp = temp->next;
             i++;
@@ -135,7 +132,75 @@ void SortList(person **head)
     Print(&head1);
 }
 
- 
+
+void save_to_file(person **head)
+{
+    FILE *fp;
+    int ret = 0;
+    fp = fopen("./linked_list_DB.txt","w+");
+    if(fp == NULL){
+        printf("File opening gets failed\n");
+        exit(0);
+    }
+    int i = 0;
+    person * temp = *head;
+    while( temp != NULL){
+        printf("i    ==========> %d\n", i);
+        ret = fwrite(temp,sizeof(person),1,fp);
+        if(ret == 0){
+            break;
+        }
+        i++;
+        temp = temp->next;
+    }
+    fclose(fp);
+}
+
+void read_from_file(person **head)
+{
+    printf("reading from file\n");
+    person *head1 = *head;
+    person *temp = NULL;
+    FILE *fp = NULL;
+    int ret = 0;
+    int i =0;
+    fp = fopen("./linked_list_DB.txt","r+");
+    if(fp == NULL){
+        printf("Error in opening the file\n");
+        exit(0);
+    }
+
+    temp = *head;
+    person *f = NULL;
+    while(!feof(fp)){
+        f = calloc(1,sizeof(person));  
+        if(f == NULL){
+            printf("Calloc_fails\n");
+            exit(0);
+        }
+        ret = fread(f,sizeof(person), 1, fp);
+        if(ret == 0){
+            break;
+        }
+        f->next = NULL;
+        printf("The value from file is %s\n",f->name); 
+        if(temp == NULL){
+            *head= f;
+            temp = *head;
+            printf("i = %d, value %s\n",i, temp->name);
+            i++;
+        }else{
+            while(temp->next != NULL){
+                printf("i = %d, value %s\n",i, temp->name);
+                i++;
+                temp = temp->next;
+            }
+        temp->next = f;
+        }
+    }
+}
+
+
 /* MAIN*/
 int main (int argc, const char * argv[])
 {
@@ -148,6 +213,8 @@ int main (int argc, const char * argv[])
     	printf("\n 1.) Insert");
     	printf("\n 2.) Print");
     	printf("\n 3.) Sort");
+        printf("\n 4.) Save_to_file");
+        printf("\n 5.) Read_from_file");
     	printf("\n 0.) EXIT");
     	printf("\nChoose ---------> ");
 
@@ -162,6 +229,12 @@ int main (int argc, const char * argv[])
                 break;
             case 3:
                 SortList(&head);
+                break;
+            case 4: 
+                save_to_file(&head);
+                break;
+            case 5:
+                read_from_file(&head);
                 break;
             case 0:
                 exit(0);
