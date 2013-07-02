57240cf3dec7c7a55d84b993d1a29d87660f0d95
Prak <www.prak.co.in@gmail.com>
2013-07-02 08:17:22 +0530

    Added the own implementaion of strstr in C

---
+++ C/strstr_implementation.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+char* my_strstr(char *s2, char *s1)
+{
+    int i, j;
+    int flag = 0;
+
+    if ((s2 == NULL || s1 == NULL)){ 
+        return NULL;
+    }
+    for( i = 0; s2[i] != '\0'; i++){
+        if (s2[i] == s1[0]){
+            for (j = i; ; j++){
+                if (s1[j-i] == '\0'){ 
+                    flag = 1; break;
+                }
+                if (s2[j] == s1[j-i]) { 
+                    continue;
+                } else { 
+                    break;
+                }
+            }
+            if (flag == 1) { 
+                break;
+            }
+        }
+    }
+
+    if (flag) { 
+        return (s2+i);
+    } else {
+        return NULL;
+    }
+}
+
+int main()
+{
+    char s2[] = "This is the statement";
+    char s1[] = "hello";
+    char *temp;
+
+    temp = my_strstr(s2,s1);
+
+    if(temp == NULL ){
+        printf("[%s] is not subset of [%s]\n", s1, s2);
+    } else {
+        printf("[%s] is a subset of [%s]\n", s1, s2);
+        printf("%s\n",temp);
+    }
+    strcpy(s1, "the");
+    temp = my_strstr(s2,s1);
+
+    if(temp == NULL ){
+        printf("[%s] is not subset of [%s]\n", s1, s2);
+    } else {
+        printf("[%s] is a subset of [%s]\n", s1, s2);
+        printf("%s\n",temp);
+    }
+
+    return 0;
+}
+
+
