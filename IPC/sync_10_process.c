fc0acbf24b5c47080fa5a0087104ebc1b5f56d98
Prak <www.prak.co.in@gmail.com>
2013-07-08 00:06:44 +0530

    Added a test program to test synchronisation

--- IPC/sync_10_process.c
+++ IPC/sync_10_process.c
@@ -1,17 +1,36 @@
-/* A program where we a create a  10 child process 
+/*AIM:
+ A program where we a create a  10 child process 
 and we have to print the process in syncronous order */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
-int main(void){
+int main (int argc, char *argv[]) {
+    int i;
+    pid_t pid;
 
-
-    
-
-    return 0
+    if( argc != 2 ){
+        printf("Usage: ./a.out <number of process>\n");
+        exit(0);
+    }
+    for (i = 1; i <= atoi(argv[1]); i++) {
+        pid = fork();
+        if (pid == -1) {
+           perror("Fork()\n");
+           return;
+        }
+        if (pid == 0) {
+           printf("I am a child2: %d PID: %d\n",i, getpid());
+            /*
+                TODO: have to create some 
+                synchronization method between these two process
+            */
+        
+           sleep (2);
+           return;
+        }
+    }
+    return 0;
 }
-
-
-
-
-
