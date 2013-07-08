8d4ac5a7ed07ad4bdcf23bc160f030ef2318333f
Prak <www.prak.co.in@gmail.com>
2013-07-08 09:27:52 +0530

    CAdded some checking condition

--- IPC/sync_10_process.c
+++ IPC/sync_10_process.c
@@ -4,33 +4,50 @@ and we have to print the process in syncronous order */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+#define MAX_PROCESS 10
 
 int main (int argc, char *argv[]) {
-    int i;
-    pid_t pid;
+    int i, j;
+    int status;
+    pid_t pid[MAX_PROCESS] ;
+    int ret;
 
     if( argc != 2 ){
         printf("Usage: ./a.out <number of process>\n");
         exit(0);
     }
+    if( atoi(argv[1]) > 10 ){
+        printf("Maximum process supporeted is 10\n");
+        exit(0);
+    }
     for (i = 1; i <= atoi(argv[1]); i++) {
-        pid = fork();
-        if (pid == -1) {
+        ret = fork();
+        if (ret == -1) {
            perror("Fork()\n");
            return;
         }
-        if (pid == 0) {
-           printf("I am a child2: %d PID: %d\n",i, getpid());
+        if (ret == 0) {
+           printf("I am a child: %d PID: %d\n",i, getpid());
+           pid[i] = getpid();
             /*
                 TODO: have to create some 
                 synchronization method between these two process
             */
         
-           sleep (2);
            return;
         }
+#if 0 
+        else {
+            for (j =0 ; j< atoi(argv[1]) ; j++){
+                waitpid(pid[j],NULL,WUNTRACED);
+            }
+        }
+#endif
+            
     }
     return 0;
 }
