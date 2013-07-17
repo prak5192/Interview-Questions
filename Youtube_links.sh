aa0710a25a4d934f8152a6a8324bd1eb746d0702
Prak <www.prak.co.in@gmail.com>
2013-07-17 07:01:30 +0530

    Upadted the download status

--- Youtube_links.sh
+++ Youtube_links.sh
@@ -2,49 +2,48 @@
 
 # All C++  lecture from youtube
 
-youtube-dl -o lecture1     http://www.youtube.com/watch?v=CzWZYwOvrcE 
-youtube-dl -o lecture2     http://www.youtube.com/watch?v=M84YRbauiwY 
-youtube-dl -o lecture3     http://www.youtube.com/watch?v=kU5elVFGglA 
-youtube-dl -o lecture4     http://www.youtube.com/watch?v=B2P2nwSvctE 
-youtube-dl -o lecture5     http://www.youtube.com/watch?v=2O0nbyy1VH8 
-youtube-dl -o lecture6     http://www.youtube.com/watch?v=NoH9TcSjLGA 
-youtube-dl -o lecture7     http://www.youtube.com/watch?v=-rTx4CmLz4o 
-youtube-dl -o lecture8     http://www.youtube.com/watch?v=qeWWy9HDKq0 
-youtube-dl -o lecture9     http://www.youtube.com/watch?v=lRlDUz6UbzE 
-youtube-dl -o lecture10    http://www.youtube.com/watch?v=aB38LFOpTZo 
-youtube-dl -o lecture11    http://www.youtube.com/watch?v=5ekF7-k6Nmc 
-youtube-dl -o lecture12    http://www.youtube.com/watch?v=03EO53mWS_c 
-youtube-dl -o lecture13    http://www.youtube.com/watch?v=7QijQNqoA84 
-youtube-dl -o lecture14    http://www.youtube.com/watch?v=uVa7hRBrGLI 
-youtube-dl -o lecture15    http://www.youtube.com/watch?v=Oaw2G4KGA9w 
-youtube-dl -o lecture16    http://www.youtube.com/watch?v=psg0erQ2u2Q 
-youtube-dl -o lecture17    http://www.youtube.com/watch?v=4bS0ut0sjGw 
+ # [ Done ]  #  youtube-dl -o lecture1     http://www.youtube.com/watch?v=CzWZYwOvrcE 
+ # [ Done ]  #  youtube-dl -o lecture2     http://www.youtube.com/watch?v=M84YRbauiwY 
+ # [ Done ]  #  youtube-dl -o lecture3     http://www.youtube.com/watch?v=kU5elVFGglA 
+ # [ Done ]  #  youtube-dl -o lecture4     http://www.youtube.com/watch?v=B2P2nwSvctE 
+ # [ Done ]  #  youtube-dl -o lecture5     http://www.youtube.com/watch?v=2O0nbyy1VH8 
+ # [ Done ]  #  youtube-dl -o lecture6     http://www.youtube.com/watch?v=NoH9TcSjLGA 
+ # [ Done ]  #  youtube-dl -o lecture7     http://www.youtube.com/watch?v=-rTx4CmLz4o 
+ # [ Done ]  #  youtube-dl -o lecture8     http://www.youtube.com/watch?v=qeWWy9HDKq0 
+ # [ Done ]  #  youtube-dl -o lecture9     http://www.youtube.com/watch?v=lRlDUz6UbzE 
+ # [ Done ]  #  youtube-dl -o lecture10    http://www.youtube.com/watch?v=aB38LFOpTZo 
+ # [ Done ]  #  youtube-dl -o lecture11    http://www.youtube.com/watch?v=5ekF7-k6Nmc 
+ # [ Done ]  #  youtube-dl -o lecture12    http://www.youtube.com/watch?v=03EO53mWS_c 
+ # [ Done ]  #  youtube-dl -o lecture13    http://www.youtube.com/watch?v=7QijQNqoA84 
+ # [ Done ]  #  youtube-dl -o lecture14    http://www.youtube.com/watch?v=uVa7hRBrGLI 
+ # [ Done ]  #  youtube-dl -o lecture15    http://www.youtube.com/watch?v=Oaw2G4KGA9w 
+ # [ Done ]  #  youtube-dl -o lecture16    http://www.youtube.com/watch?v=psg0erQ2u2Q 
+ # [ Done ]  #  youtube-dl -o lecture17    http://www.youtube.com/watch?v=4bS0ut0sjGw 
 
 
 ## Pointer lecture
-youtube-dl -o pointers  http://www.youtube.com/watch?v=Rxvv9krECNw 
+ # [ Done ]    youtube-dl -o pointers  http://www.youtube.com/watch?v=Rxvv9krECNw 
 
 ## C  binary Tree
-youtube-dl -o BST1  http://www.youtube.com/watch?v=sqVefIEttT0&list=PLC3855D81E15BC990 
+ # [ Done ]  #  youtube-dl -o BST1  http://www.youtube.com/watch?v=sqVefIEttT0&list=PLC3855D81E15BC990 
 
 # Linux common network utitlities
-# [ Done ] # youtube-dl -o network_utilites  http://www.youtube.com/watch?v=CigJXmUYXJY 
+ # [ Done ]   # youtube-dl -o network_utilites  http://www.youtube.com/watch?v=CigJXmUYXJY 
 
 ## Understanding the file system
-# [ Done ] youtube-dl -o file_system  http://www.youtube.com/watch?v=qkXh-WMxH54 
+ # [ Done ] # youtube-dl -o file_system  http://www.youtube.com/watch?v=qkXh-WMxH54 
 
 # Understanding L3 works
-youtube-dl -o L3_1    http://www.youtube.com/watch?v=i4hDKcUi-bA&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
-youtube-dl -o L3_2    http://www.youtube.com/watch?v=B2g45UGav08&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
-youtube-dl -o L3_3    http://www.youtube.com/watch?v=AuuiNCIWuPo&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
-youtube-dl -o L3_4     http://www.youtube.com/watch?v=VCrxHFmiwuA&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
+ # [ Done ]    youtube-dl -o L3_1    http://www.youtube.com/watch?v=i4hDKcUi-bA&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
+ # [ Done ]    youtube-dl -o L3_2    http://www.youtube.com/watch?v=B2g45UGav08&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
+ # [ Done ]    youtube-dl -o L3_3    http://www.youtube.com/watch?v=AuuiNCIWuPo&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
+ # [ Done ]    youtube-dl -o L3_4     http://www.youtube.com/watch?v=VCrxHFmiwuA&list=PLoJlfBTPUVl2K70s0SQxO-C4N7q7vPvv7 
 
 # Routing protocol
 
-youtube-dl -o Dynamic_routing_protocol https://www.youtube.com/watch?v=tFdkhMix-SY
-youtube-dl -o IP_multicasting https://www.youtube.com/watch?v=El_1HzhCiIg&feature=c4-overview&playnext=1&list=TLD5-1buJ8FiI
-youtube-dl -o open_shorted_path https://www.youtube.com/watch?v=qZAmrTFzXAo&feature=c4-overview&playnext=1&list=TLeGvGJFW7YNg
-youtube-dl -o Understanding_routing_logic1 https://www.youtube.com/watch?v=2CAkMS3bDYE&list=UUCHV1ycPmvKeiRrlqnv1kRA
-youtube-dl -o Understanding_routing_logic2 https://www.youtube.com/watch?v=AhRCjcXvVDA
-youtube-dl -o Introduction_to_RIP https://www.youtube.com/watch?v=0efXawUgNZg
-
+ # [ Done ]  #     youtube-dl -o Dynamic_routing_protocol https://www.youtube.com/watch?v=tFdkhMix-SY
+ # [ Done ]  #     youtube-dl -o IP_multicasting https://www.youtube.com/watch?v=El_1HzhCiIg&feature=c4-overview&playnext=1&list=TLD5-1buJ8FiI
+ # [ Done ]  #     youtube-dl -o open_shorted_path https://www.youtube.com/watch?v=qZAmrTFzXAo&feature=c4-overview&playnext=1&list=TLeGvGJFW7YNg
+ # [ Done ]  #     youtube-dl -o Understanding_routing_logic1 https://www.youtube.com/watch?v=2CAkMS3bDYE&list=UUCHV1ycPmvKeiRrlqnv1kRA
+ # [ Done ]  #     youtube-dl -o Understanding_routing_logic2 https://www.youtube.com/watch?v=AhRCjcXvVDA
+ # [ Done ]  #     youtube-dl -o Introduction_to_RIP https://www.youtube.com/watch?v=0efXawUgNZg
