1ba4562037f310ff1b6ce391770c754ecae1e86c
Prak <www.prak.co.in@gmail.com>
2013-07-19 07:12:34 +0530

    Added a more mcafee question

---
+++ C/mcafee_question3.c
@@ -0,0 +1,11 @@
+/*
+
+Normal bubble sort require 100sec to sort 10000 entries.what 
+will be the input size if it will solve in 50sec.
+
+*/
+
+Its complexity is O(n^2). Means processor is doing 10000^2, i.e. 
+10^8 steps in 100 sec. In 50 Sec it will do 5*10^7 steps. Sqrt 
+of it is approx 7000. Hence input size is 7k
+
