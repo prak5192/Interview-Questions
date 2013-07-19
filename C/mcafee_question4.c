fd4ab02e4835530f593e3a6e6ac8ca5089c5c35e
Prak <www.prak.co.in@gmail.com>
2013-07-19 07:48:58 +0530

    Added one more solution

--- C/mcafee_question4.c
+++ C/mcafee_question4.c
@@ -1,18 +1,35 @@
+#if 0
 /*
     Write a program to print string in the following pattern -
 If i/p is 'abc' then o/p should be 'abbccccccbba'.
+
 */
 
-/* Non -tested solution * /
+/* 
+Non -tested solution 
+* /
+# endif
 
-public static void main(String[] args) {
-String input = "abc";
-int length = input.length();
-StringBuilder str = new StringBuilder();
-for (int i = 0; i < length; i++) {
-for (int j = 0; j < i+1; j++) {
-str.append(input.charAt(i));
-}
-}
-System.out.println(str.toString()+str.reverse().toString());
+char *pattern_convertion(char *str)
+{
+    char *ret = NULL;
+    int l = strlen(str);
+    if(l == 0) return NULL;
+    if((ret = (char *)malloc(l*sizeof(char))) == NULL) return NULL;
+    
+    int c = 0, i,j;
+    for(i = 0; i<l; i++)
+    {
+        for(j = 0; j<i+1; j++, c++)
+            ret[c] = str[i];
+    }
+
+    for(i = l-1; i>=0; i--)
+    {
+        for(j = 0; j<=l; j--, c++)
+            ret[c] = str[i];
+    }
+    str[c] = '\0';
+    return str;
+
 }
