69f9447b6dc5af3b2de7d470dc1edced254622c5
Prak <www.prak.co.in@gmail.com>
2013-07-19 06:56:20 +0530

    Added a google question

---
+++ C/google1.c
@@ -0,0 +1,9 @@
+/*
+http://stackoverflow.com/questions/17591467/find-the-biggest-interval-that-has-all-its-members-in-list-in-on/17592550#17592550
+
+
+Given a list of integers, find out the biggest interval that has 
+all its members in the given list. e.g. given list 1, 7, 4, 6, 3, 10, 2 
+then answer would be [1, 4]. Develop algorithm and write code for this
+
+*/
