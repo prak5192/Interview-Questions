8a7b62543026d2e2fc0ce452ea46d6b0ae500700
Prak <www.prak.co.in@gmail.com>
2013-07-30 07:23:26 +0530

    Added a question on singliy linked list

---
+++ C/find_3rd_element_from_last_in_singly_linked_list.c
@@ -0,0 +1,15 @@
+/*
+    How do you find 3rd element from last in single pass?
+*/
+
+/*
+
+Explanation:
+    This can be solved by using 2 pointers, start second 
+pointer when first pointer reaches third place. So when the 
+1st pointer reaches the end of the list, the second will reach 
+to the 3rd element from the last of the singly list.
+
+*/
+
+
